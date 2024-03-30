b60860226f91dbc02058372d8ee0dd2fa33f2aad
Wardan Ahmed <165463281+Wardan2009@users.noreply.github.com>
2024-03-30 15:54:13 +0500

    Add files via upload

---
+++ review/anda.js
@@ -0,0 +1,7 @@
+function cook(dish) {
+    console.log("making" + dish + "...");
+    console.log(dish + "is ready");
+    return dish + "khalo";
+}
+var cookedFood = cook("anda");
+console.log(cookedFood);
---
+++ review/anda.ts
@@ -0,0 +1,9 @@
+function cook (dish: string){
+    console.log("making" + dish + "...");
+    console.log(dish + "is ready");
+    return dish + "khalo";
+}
+let cookedFood = cook("anda"); 
+console.log (cookedFood);
+
+
